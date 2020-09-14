8c293d6f810e065c83b45d3dbb3740ff4ec0dc86
Anastasia Osinska <60783750+AnastasiaOsinska@users.noreply.github.com>
2020-09-14 11:22:07 +0300

    Initial Commit

---
+++ ForecastAPiTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForecastAPiTest
+//
+//  Created by Anastasiya Osinskaya on 9/14/20.
+//  Copyright © 2020 Anastasiya Osinskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
